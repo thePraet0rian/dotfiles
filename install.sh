b30267dc113bb0af9399dde9a482c549b06d0e64
the_praet0rian <146135845+thePraet0rian@users.noreply.github.com>
2025-02-21 22:58:48 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!bin/bash
+
+# // INSTALLING PROGRAMS ////////////////////////
+
+# Installs version controll
+sudo apt install git
+sudo apt install gh
+
+# Random stuff
+sudo apt install neofetch 
+sudo apt install btop
+sudo apt install curl
+
+# Installs browser
+sudo apt install firefox-esr
+
+# Installs i3 
+sudo apt install i3
+sudo apt install feh
+sudo apt install rofi
+sudo apt install gnome-network-manager
+sudo apt install ibus
+
+# Installs Terminator 
+sudo apt install terminator
+# should install fonts
+
+# Godot 
+sudo curl https://github.com/godotengine/godot/releases/download/4.3-stable/Godot_v4.3-stable_linux.x86_64.zip
+unzip Godot_v4.3-stable_linux.x86_64.zip
+mv Godot_v4.3-stable_linux.x86_64 godot
+
+# Aseprite
+
+# Discord
+sudo curl https://discord.com/api/download?platform=linux&format=deb
+
+# // CONFIGURING PROGRAMMS //////////////////////
